e2e578ddb043a829046d96e8ce3c8e2360724c39
Toyosi <aminatusman002@gmail.com>
2024-10-06 13:37:28 -0400

    Production Ready

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,13 @@ from app.aws_cred import aws_credentials_bp
 from app.ec2 import ec2_bp
 from app.scan import scan_bp
 from app.secgroup import security_bp
+from flask_cors import CORS
+
 
 # Create the Flask app
 def create_app():
     app = Flask(__name__)
+    CORS(app)
 
     # Register blueprints
     app.register_blueprint(aws_credentials_bp)
